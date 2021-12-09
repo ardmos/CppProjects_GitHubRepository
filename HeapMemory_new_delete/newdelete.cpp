22b1a76ee104f12876af3c690d36e32c9c0cc83f
ardmos <46891528+ardmos@users.noreply.github.com>
2021-12-09 21:40:43 +0900

    Heap 메모리에 배열 할당하기

--- HeapMemory_new_delete/newdelete.cpp
+++ HeapMemory_new_delete/newdelete.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+/* 1. Heap 메모리에 변수 할당하기
 int main(){
     int* p = new int;
     *p = 10;
@@ -9,3 +10,23 @@ int main(){
     delete p;
     return 0;
 }
+*/
+
+// 2. Heap 메모리에 배열 할당하기
+int main(){
+    int arr_size;
+    std::cout << "array size : ";
+    std::cin >> arr_size;
+    int *list = new int[arr_size];
+    for (int i = 0; i < arr_size; i++)
+    {
+        std::cin >> list[i];
+    }
+    for (int i = 0; i < arr_size; i++)
+    {
+        std::cout << i << "th element of list : " << list[i] << std::endl;
+    }
+    delete[] list;
+    return 0;    
+}
+
